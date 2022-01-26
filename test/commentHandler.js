357f5544834e8e7d13d6a8908dc6b0bad48f6ea6
Brett Zamir <brettz9@yahoo.com>
2022-01-26 08:28:30 +0800

    test: add `commentHandler` tests

---
+++ test/commentHandler.js
@@ -0,0 +1,44 @@
+import {commentHandler} from '../src/index.js';
+import {parseComment} from '../src/parseComment.js';
+
+describe('commentHandler', function () {
+  it('Returns `true` with match', function () {
+    const handler = commentHandler({
+      mode: 'typescript'
+    });
+
+    const parsed = parseComment({
+      value: `* @property opt_a
+ * @param {Bar|Foo} opt_b
+`
+    });
+
+    const result = handler(
+      'JSDocBlock:has(JSDocTag[tag="param"][name=/opt_/] > ' +
+      'JSDocTypeUnion:has(JsdocTypeName[value="Bar"]:nth-child(1)))',
+      parsed
+    );
+
+    expect(result).to.equal(true);
+  });
+
+  it('Returns `false` when no match present', function () {
+    const handler = commentHandler({
+      mode: 'typescript'
+    });
+
+    const parsed = parseComment({
+      value: `* @property opt_a
+ * @param {Foo|Bar} opt_b
+`
+    });
+
+    const result = handler(
+      'JSDocBlock:has(JSDocTag[tag="param"][name=/opt_/] > ' +
+      'JSDocTypeUnion:has(JsdocTypeName[value="Bar"]:nth-child(1)))',
+      parsed
+    );
+
+    expect(result).to.equal(false);
+  });
+});
