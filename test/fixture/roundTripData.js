bebf4f02698bbd5052cde7edaffcb87c5080956a
typhonrt <support@typhonrt.org>
2024-02-22 03:06:59 -0800

    Update round trip tests.

--- test/fixture/roundTripData.js
+++ test/fixture/roundTripData.js
@@ -16,45 +16,81 @@ const commentBlocks = [];
 const compactResults = [];
 
 /**
+ * Defines the results for `compact` option w/ Pratt types.
+ * The Pratt parser compacts multiline union types.
+ *
+ * @type {(string|undefined)[]}
+ */
+const compactResultsPratt = [];
+
+/**
  * Defines the results for `preserve` option.
  *
  * @type {(string|undefined)[]}
  */
 const preserveResults = [];
 
+/**
+ * Defines the results for `preserve` option w/ Pratt types.
+ * The Pratt parser compacts multiline union types.
+ *
+ * @type {(string|undefined)[]}
+ */
+const preserveResultsPratt = [];
+
 // Normal single / multi-line comments (no types) -----------------------------
 
 // commentBlock[0] ----
 
 commentBlocks.push(`/** Should parse */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[1] ----
 
 commentBlocks.push(`/** @singleTag */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[2] ----
 
 commentBlocks.push(`/** @param foo - with description */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[3] ----
 
 commentBlocks.push(`/** This is {@link Something} */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[4] ----
 
 commentBlocks.push(`/**
  * Should parse
  */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[5] ----
 
@@ -63,19 +99,27 @@ commentBlocks.push(`/**
  *
  * with empty line.
  */`);
+
 compactResults.push(`/**
  * Should parse
  * with empty line.
  */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[6] ----
 
 commentBlocks.push(`/**
  * @param foo - description no type.
  */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[7] ----
 
@@ -89,7 +133,10 @@ compactResults.push(`/**
  * @param foo
  * description starting on new line.
  */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[8] ----
 
@@ -98,11 +145,15 @@ commentBlocks.push(`/**
  *
  * with empty lines.
  */`);
+
 compactResults.push(`/**
  * @param foo - description no type
  * with empty lines.
  */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[9] ----
 
@@ -111,11 +162,15 @@ commentBlocks.push(`/**
  *
  * @param bar - description no type.
  */`);
+
 compactResults.push(`/**
  * @param foo - description no type.
  * @param bar - description no type.
  */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[10] ----
 
@@ -128,6 +183,7 @@ commentBlocks.push(`/**
  * @param bar
  * description no type.
  */`);
+
 compactResults.push(`/**
  * @param foo
  * no type description spread
@@ -135,15 +191,22 @@ compactResults.push(`/**
  * @param bar
  * description no type.
  */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // Normal single / multi-line comments (with types) ---------------------------
 
 // commentBlock[11] ----
 
 commentBlocks.push(`/** @param {boolean} foo - with description */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[12] ----
 
@@ -155,20 +218,28 @@ commentBlocks.push(`/**
  * @param {boolean} and tag
  *
  */`);
+
 compactResults.push(`/**
  * Should parse
  * with empty line.
  * @param {boolean} and tag
  */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[13] ----
 
 commentBlocks.push(`/**
  * @param {boolean | string} foo - multiple types
  */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[14] ----
 
@@ -178,8 +249,16 @@ commentBlocks.push(`/**
  *    string
  * )} foo - multiple types across lines w/ parens
  */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(`/**
+ * @param {(boolean | string)} foo - multiple types across lines w/ parens
+ */`);
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(`/**
+ * @param {(boolean | string)} foo - multiple types across lines w/ parens
+ */`);
 
 // commentBlock[15] ----
 
@@ -191,6 +270,7 @@ commentBlocks.push(`/**
  *
  * and extended tag description.
  */`);
+
 compactResults.push(`/**
  * @param {(
  *    boolean |
@@ -198,7 +278,17 @@ compactResults.push(`/**
  * )} foo - multiple types across lines w/ parens
  * and extended tag description.
  */`);
+compactResultsPratt.push(`/**
+ * @param {(boolean | string)} foo - multiple types across lines w/ parens
+ * and extended tag description.
+ */`);
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(`/**
+ * @param {(boolean | string)} foo - multiple types across lines w/ parens
+ *
+ * and extended tag description.
+ */`);
 
 // commentBlock[16] ----
 
@@ -212,6 +302,7 @@ commentBlocks.push(`/**
  *
  * and extended tag description.
  */`);
+
 compactResults.push(`/**
  * @param {(
  *    boolean |
@@ -220,45 +311,108 @@ compactResults.push(`/**
  * tag description staring with gaps
  * and extended tag description.
  */`);
-preserveResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(`/**
+ * @param {(boolean | string)} foo
+ * tag description staring with gaps
+ * and extended tag description.
+ */`);
 
-// Irregular spacing spacing --------------------------------------------------
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(`/**
+ * @param {(boolean | string)} foo
+ *
+ * tag description staring with gaps
+ *
+ * and extended tag description.
+ */`);
 
 // commentBlock[17] ----
 
 commentBlocks.push(`/**
- *    @hidden
+
+ * @param {string} foo
+ *
+ * tag description staring with gaps
+ *
+ * and extended tag description.
  */`);
-compactResults.push(commentBlocks.at(-1));
+compactResults.push(`/**
+ * @param {string} foo
+ * tag description staring with gaps
+ * and extended tag description.
+ */`);
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // commentBlock[18] ----
 
 commentBlocks.push(`/**
- *    @param indented
+ * @param {string} foo
+ * tag description staring with gaps
+ * and extended tag description.
  */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// Irregular spacing spacing --------------------------------------------------
 
 // commentBlock[19] ----
+
+commentBlocks.push(`/**
+ *    @hidden
+ */`);
+
+compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[20] ----
+
+commentBlocks.push(`/**
+ *    @param indented
+ */`);
+
+compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[21] ----
 
 commentBlocks.push(`/**
  *    @param {boolean} foo -
  *    description indented
  */`);
-compactResults.push(commentBlocks.at(-1));
-preserveResults.push(commentBlocks.at(-1));
 
-// commentBlock[20] ----
+compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[22] ----
 
 commentBlocks.push(`/**
  *    @param foo -
  *    description indented
  */`);
-compactResults.push(commentBlocks.at(-1));
-preserveResults.push(commentBlocks.at(-1));
 
-// commentBlock[21] ----
+compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[23] ----
 
 commentBlocks.push(`/**
  *    @param foo -
@@ -267,72 +421,99 @@ commentBlocks.push(`/**
  *   @param bar -
  *  description indented
  */`);
+
 compactResults.push(`/**
  *    @param foo -
  *   description indented
  *   @param bar -
  *  description indented
  */`);
-preserveResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
 
-// commentBlock[22] ----
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[24] ----
 
 commentBlocks.push(`/**
  *  @param foo -
  *  description
  no delimiter
  */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // Irregular opening / closing lines & delimiter spacing ----------------------
 
-// commentBlock[23] ----
+// commentBlock[25] ----
 
 commentBlocks.push(`/**
  * Should parse one line. */`);
 
 compactResults.push(commentBlocks.at(-1));
-preserveResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
 
-// commentBlock[24] ----
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[26] ----
 
 commentBlocks.push(`/**
  *    Should parse one line with extra space.   */`);
 
 compactResults.push(commentBlocks.at(-1));
-preserveResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
 
-// commentBlock[25] ----
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[27] ----
 
 commentBlocks.push(`/** It should
  *
  * parse */`);
+
 compactResults.push(`/** It should
  * parse */`);
-preserveResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
 
-// commentBlock[26] ----
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
+
+// commentBlock[28] ----
 
 commentBlocks.push(`/** It should
  * @tagOnLastLine */`);
+
 compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
 preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 // Technically invalid comment blocks that parse correctly --------------------
 
-// commentBlock[27] ----
+// commentBlock[29] ----
 
 commentBlocks.push(`/**
  * @param
  * foo - no param name / just description.
  */`);
-compactResults.push(commentBlocks.at(-1));
-preserveResults.push(commentBlocks.at(-1));
 
+compactResults.push(commentBlocks.at(-1));
+compactResultsPratt.push(compactResults.at(-1));
+
+preserveResults.push(commentBlocks.at(-1));
+preserveResultsPratt.push(preserveResults.at(-1));
 
 export {
   commentBlocks,
   compactResults,
-  preserveResults
+  compactResultsPratt,
+  preserveResults,
+  preserveResultsPratt
 };
--- test/roundTripParsing.test.js
+++ test/roundTripParsing.test.js
@@ -7,7 +7,9 @@ import {
 import {
   commentBlocks,
   compactResults,
-  preserveResults
+  compactResultsPratt,
+  preserveResults,
+  preserveResultsPratt
 } from './fixture/roundTripData.js';
 
 // The goal in the following data defined tests is round trip testing from
@@ -15,6 +17,26 @@ import {
 // `commentBlocks` provides the source comment block and `compactResults`
 // `preserveResults` provides the respective results for `compact` and
 // `preserve` `spacing` option for `commentParserToESTree`.
+
+/**
+ * Converts `JsdocDescriptionLine[]` to a string description removing
+ * any leading white space or delimiter.
+ *
+ * @param {(
+ *   import('../src/commentParserToESTree.js').JsdocDescriptionLine[])
+ * } descriptionLines -
+ * @returns {string} Complete description line string.
+ */
+const descriptionLinesToString = (descriptionLines) => {
+  let result = '';
+
+  for (const descriptionLine of descriptionLines) {
+    const desc = estreeToString(descriptionLine).replace(/^\s*\**\s*/u, '');
+    result += !result ? desc : `\n${desc}`;
+  }
+
+  return result;
+};
 
 describe('Round Trip Parsing', () => {
   describe('`compact` option', () => {
@@ -25,9 +47,25 @@ describe('Round Trip Parsing', () => {
 
         const ast = commentParserToESTree(parsedComment, 'jsdoc');
 
-        const result = estreeToString(ast);
+        // Check description lines against `ast.description`.
+        expect(ast.description).to.equal(
+          descriptionLinesToString(ast.descriptionLines)
+        );
+
+        // Check tag description lines against `tag.description`.
+        for (const tag of ast.tags) {
+          expect(tag.description).to.equal(
+            descriptionLinesToString(tag.descriptionLines)
+          );
+        }
+
+        const result = estreeToString(ast, {preferRawType: true});
 
         expect(result).to.equal(compactResults[i]);
+
+        const resultPratt = estreeToString(ast);
+
+        expect(resultPratt).to.equal(compactResultsPratt[i]);
       });
     }
   });
@@ -41,9 +79,23 @@ describe('Round Trip Parsing', () => {
         const ast = commentParserToESTree(parsedComment, 'jsdoc',
           {spacing: 'preserve'});
 
-        const result = estreeToString(ast);
+        expect(ast.description).to.equal(
+          descriptionLinesToString(ast.descriptionLines)
+        );
+
+        for (const tag of ast.tags) {
+          expect(tag.description).to.equal(
+            descriptionLinesToString(tag.descriptionLines)
+          );
+        }
+
+        const result = estreeToString(ast, {preferRawType: true});
 
         expect(result).to.equal(preserveResults[i]);
+
+        const resultPratt = estreeToString(ast);
+
+        expect(resultPratt).to.equal(preserveResultsPratt[i]);
       });
     }
   });
